64a7c8c5c5a96a658d73bfb035386cbf3bba49a1
Cristian <kristian13orellanagalaz@gmail.com>
2023-07-10 17:13:57 -0400

    second commit

---
+++ app/src/main/java/com/example/mantenedorautomoviles/GestorDB.java
@@ -0,0 +1,9 @@
+package com.example.mantenedorautomoviles;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public class GestorDB {
+
+}
